0fc308ef2b3086494bba067a16e00464f49f08cf
xavenordu <xavenordu@gmail.com>
2026-02-11 11:14:05 +0100

    Prod X

--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++ .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__28bc9c2a._.css",
-  "static/chunks/node_modules_b93feef0._.js",
+  "static/chunks/node_modules_04cda6a7._.js",
   "static/chunks/_0437daa2._.js"
 ],
     source: "dynamic"
